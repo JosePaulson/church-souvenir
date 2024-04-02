bcaa8a7363b53e87314172dbd44be5d42e397ccd
Paulson Jose <paul@hackermail.com>
2024-04-02 20:48:04 +0530

    Prod build - 1

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ import express from 'express'
 import connectDB from './config/mongoDBConfig.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
+// import cors from 'cors'
 import housesRoutes from './routes/housesRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import { notFound, errorHandler } from './middlewares/errorHandler.js'
+import path from 'path'
 
 
 dotenv.config()
@@ -15,21 +16,28 @@ connectDB()
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+// }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.get('/', (req, res)=>{
-    res.send('server started: PORT-' + port)
-})
+// app.get('/', (req, res)=>{
+//     res.send('server started: PORT-' + port)
+// })
 
 app.use('/api/houses', housesRoutes)
 app.use('/api/auth', authRoutes)
+
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use(notFound)
 app.use(errorHandler)
