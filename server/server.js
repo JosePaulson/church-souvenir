7d00c77f604f2550a7505841de312716d0d6e2d2
Paulson Jose <paul@hackermail.com>
2024-04-02 23:08:39 +0530

    cors edition

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import connectDB from './config/mongoDBConfig.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-// import cors from 'cors'
+import cors from 'cors'
 import housesRoutes from './routes/housesRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import { notFound, errorHandler } from './middlewares/errorHandler.js'
@@ -16,10 +16,10 @@ connectDB()
 
 const app = express()
 
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true,
-// }))
+app.use(cors({
+    origin: 'https://church-souvenir.vercel.app',
+    credentials: true,
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -32,12 +32,12 @@ app.use(cookieParser())
 app.use('/api/houses', housesRoutes)
 app.use('/api/auth', authRoutes)
 
-// const __dirname = path.resolve()
-// app.use(express.static(path.join(__dirname, '/client/dist')))
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/client/dist')))
 
-// app.get('*', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-// })
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use(notFound)
 app.use(errorHandler)
